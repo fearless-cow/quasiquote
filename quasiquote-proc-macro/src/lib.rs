d0d66fefca818da58cfad837fe1ee90f2d7c1e45
Fearless Cow <134126853+fearless-cow@users.noreply.github.com>
2023-04-29 21:48:28 -0400

    init

---
+++ quasiquote-proc-macro/src/lib.rs
@@ -0,0 +1,5 @@
+#[allow(unused_variables)]
+#[proc_macro]
+pub fn quasiquote(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    todo!()
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub use proc_macro2;
+pub use quasiquote_proc_macro::quasiquote;
+pub use quote;
