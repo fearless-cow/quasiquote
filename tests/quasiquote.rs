88ec2ef10696680cd90a9d4ab1b7f0d3f9cd02c8
Fearless Cow <134126853+fearless-cow@users.noreply.github.com>
2023-05-07 00:30:16 -0400

    impl test for quoting w/o interpolation

---
+++ tests/quasiquote.rs
@@ -0,0 +1,42 @@
+use quasiquote::quasiquote;
+use std::fmt;
+
+fn compare_strings<A, B>(a: A, b: B)
+where
+    A: AsRef<str> + fmt::Display,
+    B: AsRef<str> + fmt::Display,
+{
+    if a.as_ref()
+        .chars()
+        .filter(|c| !c.is_ascii_whitespace())
+        .collect::<String>()
+        != b.as_ref()
+            .chars()
+            .filter(|c| !c.is_ascii_whitespace())
+            .collect::<String>()
+    {
+        panic!("strings do not match:\n{}\n{}", a, b)
+    }
+}
+
+#[test]
+fn pass_through() {
+    let a = quasiquote! {
+        fn ferris<T>(_: T) -> T {
+            let a = [1, 2, 3];
+            let b = {
+                1 + 2 + (3 + 4)
+            };
+        }
+    }
+    .to_string();
+    let b = stringify! {
+        fn ferris<T>(_: T) -> T {
+            let a = [1, 2, 3];
+            let b = {
+                1 + 2 + (3 + 4)
+            };
+        }
+    };
+    compare_strings(a, b);
+}
