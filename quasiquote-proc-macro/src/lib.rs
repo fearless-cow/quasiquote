3a658bf040bf8ca1131b822ef3bf3e783e362bff
Fearless Cow <134126853+fearless-cow@users.noreply.github.com>
2023-05-14 01:14:00 -0400

    rewrite debug macro

--- quasiquote-proc-macro/src/lib.rs
+++ quasiquote-proc-macro/src/lib.rs
@@ -1,21 +1,27 @@
-#![feature(let_chains)]
-
 pub(crate) mod quasiquote;
 
-#[allow(unused_macros)]
-macro_rules! dbgfd {
-    ($fd:literal, $($tail:tt)+) => {
-        {
-            use std::io::Write;
-            let mut file = unsafe { <std::fs::File as std::os::fd::FromRawFd>::from_raw_fd($fd) };
-            write!(file, $($tail)+).unwrap();
+#[cfg(debug_assertions)]
+macro_rules! dbg_macro {
+    ($fmt:literal, $i:expr) => {{
+        use std::io::Write;
+        if let Ok(path) = std::env::var("DEBUG_MACRO") {
+            let mut file = std::fs::File::create(path).unwrap();
+            write!(file, $fmt, $i).unwrap();
         }
-    };
+    }};
+}
+
+#[cfg(not(debug_assertions))]
+macro_rules! dbg_macro {
+    (fmt => $fmt:literal, file => $file:expr, $i:expr) => {{
+        ()
+    }};
 }
 
 #[allow(unused_variables)]
 #[proc_macro]
 pub fn quasiquote(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let output = quasiquote::expand(input.into());
+    dbg_macro!("pub fn f() {{{}}}", output.to_string());
     output.into()
 }
