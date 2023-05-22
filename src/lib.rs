068c3b47f4b711401640fb6c42924a416c679209
Fearless Cow <134126853+fearless-cow@users.noreply.github.com>
2023-05-22 02:32:14 -0400

    add documentation with examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+//! Provides a wrapper around quote! that allows interpolating arbitrary expressions.
+//!
+//! Expected syntax is identical to what's used for quote!, except that a new interpolation pattern
+//! is allowed.
+//! # Examples
+//! With quote:
+//! ```
+//! use quote::quote;
+//! use proc_macro2::TokenStream;
+//! use syn::{Field, Member};
+//!
+//! pub fn expand_getter(field: &Field) -> TokenStream {
+//!     let ident = &field.ident;
+//!     let member = Member::Named(ident.as_ref().cloned().unwrap());
+//!     let ty = &field.ty;
+//!     quote! {
+//!         pub fn #ident(&self) -> #ty {
+//!             &self.#member
+//!         }
+//!     }
+//! }
+//! ```
+//! With quasiquote:
+//! ```
+//! use quasiquote::quasiquote;
+//! use proc_macro2::TokenStream;
+//! use syn::{Field, Member};
+//!
+//! pub fn expand_getter(field: &Field) -> TokenStream {
+//!     let member = Member::Named(field.ident.as_ref().cloned().unwrap());
+//!     quasiquote! {
+//!         pub fn #{&field.ident}(&self) -> #{&field.ty} {
+//!             &self.#member
+//!         }
+//!     }
+//! }
+//! ```
 pub use proc_macro2;
 pub use quasiquote_proc_macro::quasiquote;
 pub use quote;
