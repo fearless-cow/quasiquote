c4c318b299101a3102876df921dde382c003d07c
Fearless Cow <134126853+fearless-cow@users.noreply.github.com>
2023-05-22 02:48:32 -0400

    add todos to docs

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,9 @@
 //!     }
 //! }
 //! ```
+//! # Not yet implemented
+//! * interpolation of expressions inside of repetitions
+//!
 pub use proc_macro2;
 pub use quasiquote_proc_macro::quasiquote;
 pub use quote;
