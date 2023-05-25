43a94953ab66e7637853e28915f9674d80a318a0
Fearless Cow <134126853+fearless-cow@users.noreply.github.com>
2023-05-25 03:11:54 -0400

    rewrite debug helper

--- quasiquote-proc-macro/src/lib.rs
+++ quasiquote-proc-macro/src/lib.rs
@@ -1,29 +1,40 @@
 #![deny(clippy::pedantic, clippy::use_self)]
+#![allow(clippy::missing_panics_doc)]
 
 pub(crate) mod quasiquote;
-
-#[cfg(debug_assertions)]
-macro_rules! dbg_macro {
-    ($fmt:literal, $i:expr) => {{
-        use std::io::Write;
-        if let Ok(path) = std::env::var("DEBUG_MACRO") {
-            let mut file = std::fs::File::create(path).unwrap();
-            write!(file, $fmt, $i).unwrap();
-        }
-    }};
-}
-
-#[cfg(not(debug_assertions))]
-macro_rules! dbg_macro {
-    (fmt => $fmt:literal, file => $file:expr, $i:expr) => {{
-        ()
-    }};
-}
 
 #[allow(unused_variables)]
 #[proc_macro]
 pub fn quasiquote(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let output = quasiquote::expand(input.into());
-    dbg_macro!("pub fn f() {{{}}}", output.to_string());
+    quasiquote::expand(input.into()).into()
+}
+
+#[proc_macro]
+#[cfg(debug_assertions)]
+pub fn quasiquote_dbg(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    use std::{env, fs::File, io::Write};
+    let mut dbg_input_file = File::create(
+        env::var("QUASIQUOTE_DBG_INPUT")
+            .as_deref()
+            .unwrap_or("/dev/null"),
+    )
+    .unwrap();
+    let mut dbg_ouput_file = File::create(
+        env::var("QUASIQUOTE_DBG_OUTPUT")
+            .as_deref()
+            .unwrap_or("/dev/null"),
+    )
+    .unwrap();
+    let mut display_output_file = File::create(
+        env::var("QUASIQUOTE_DISPLAY_OUTPUT")
+            .as_deref()
+            .unwrap_or("/dev/null"),
+    )
+    .unwrap();
+    let input: proc_macro2::TokenStream = input.into();
+    let output = quasiquote::expand(input.clone());
+    write!(dbg_input_file, "{input:?}").unwrap();
+    write!(dbg_ouput_file, "{output:?}").unwrap();
+    write!(display_output_file, "fn f(){{{output}}}").unwrap();
     output.into()
 }
